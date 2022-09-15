85ac6c73954c01ff99c293d8c751ff764f80237a
desguerra <ddesguerra@ucdavis.edu>
2022-09-14 21:59:57 -0700

    connect to mysql database

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+const mysql = require('mysql2');
 const express = require('express');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,14 +9,25 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Routes
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username
+        user: 'root',
+        // MySQL password
+        password: process.env.KEY,
+        database: 'election',
+    },
+    console.log('Connected to the election database.')
+);
 
+// Routes
 
 // Default response for any other request (404 Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
-
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
